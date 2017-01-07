c8b7ed702074c05471ee605a80fbfb99d07e5e28
unknown <jasmeet0915@gmail.com>
2017-01-08 00:52:22 +0530

    First commit

---
+++ algoscraper.py
@@ -0,0 +1,21 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+def algoinfo():
+    urllist=['https://en.wikipedia.org/wiki/Bubble_sort',
+         'https://en.wikipedia.org/wiki/Heapsort',
+         'https://en.wikipedia.org/wiki/Quicksort',
+         'https://en.wikipedia.org/wiki/Merge_sort',
+         'https://en.wikipedia.org/wiki/Selection_sort',
+         'https://en.wikipedia.org/wiki/Insertion_sort']
+    for x in urllist:
+        infofile=urllib2.urlopen(x)
+        infohtm=infofile.read()
+        infofile.close()
+        soup=BeautifulSoup(infohtm,'html.parser')
+        p=soup.find_all('p')
+        print p[0].text
+
+if __name__ == '__main__':
+    algoinfo()
+        

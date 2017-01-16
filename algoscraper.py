603464a42b364fd1c3877bd6273ed84c14472c73
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2017-01-16 22:51:18 +0530

    searching algo info added

--- algoscraper.py
+++ algoscraper.py
@@ -18,8 +18,20 @@ def algoinfo():
             print p[1].text
         else:
             print p[0].text
-        
+
+def searchinginfo():
+    urllist=['https://en.wikipedia.org/wiki/Binary_search_algorithm',
+             'https://en.wikipedia.org/wiki/Linear_search']
+    for i in urllist:
+        searchfile=urllib2.urlopen(i)
+        searchhtm=searchfile.read()
+        searchfile.close()
+        soup=BeautifulSoup(searchhtm,'html.parser')
+        p=soup.find_all('p')
+        print p[0].text
+    
 
 if __name__ == '__main__':
     algoinfo()
+    searchinginfo()
         
